848c3572e9fdc3bb29641ccd498d337444f10043
vlorian <vlorian.dervishi@gmail.com>
2023-09-07 11:54:14 -0400

    tests are done

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const wordPalindrome = word.split("");
+  const reverseWord = wordPalindrome.reverse().join("");
+  return word === reverseWord;
 }
 
 /* 
   Add your pseudocode here
+  -Find a word and see if its written the same when written backwards.
 */
 
 /*
   Add written explanation of your solution here
+  1.Get word as a string from (word)parameter 
+  2.Convert word and split into each character as an array.
+  3.Use array method reverse() to reverse the order of the characters.
+  4.Use array method join() to convert array of characters back into a word.
+  5.Compare initial word with reverseWord
+  6.return the boolean value of the comparison after the function has completed execution.
 */
 
 // You can run `node index.js` to view these console logs
